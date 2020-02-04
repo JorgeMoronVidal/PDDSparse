7a6f64671a30299448677889d07f7935b3185551
JorgeMoronVidal <jorgemoronvidal@gmail.com>
2020-02-04 16:14:42 +0100

    Changes to be committed: 	modified:   BVP.hpp 	modified:   node.hpp

--- BVP.hpp
+++ BVP.hpp
@@ -6,7 +6,7 @@
 #include <string>
 #include <eigen3/Eigen/Core>
 #include "lookuptable.hpp"
-#include "surface.hpp"
+#include "boundary.hpp"
 
 /*
  BVP stores the functions that can be used to compute the solution of 
@@ -19,39 +19,46 @@ class BVP
     typedef Eigen::VectorXf (*pfvector)(Eigen::VectorXf);
     typedef Eigen::VectorXf (*pfmatrix)(Eigen::MatrixXf);
     typedef float (*pfbound)(float* params, Eigen::VectorXf &, Eigen::VectorXf &);
+    typedef bool (*pfstop)(Eigen::MatrixXf);
 
     private:
 
         //Default functions which always return 0.0f
         float Default_Scalar(Eigen::VectorXf);
         Eigen::VectorXf Default_Vector(Eigen::VectorXf);
-
+        Eigen::MatrixXf Default_Matrix(Eigen::VectorXf);
         //LookAtTable stores the LookAtTable of those functions that are not defined analytically
         std::map<std::string, LookUpTable> bvplat;
 
     public:
 
-        //bvpscalar stores the functions whose image is scalar s.t. c(X), f(X), u(X), varphi(X)
-        std::map<std::string, pfscalar> bvpscalar;
+        //Functions whose image is scalar s.t. c(X), f(X), u(X), varphi(X)
+        pfscalar f,c, u, varphi;
 
-        //bvpvector stores the functions whose image is a vector s.t b(X), F(x), g(X)
-        std::map<std::string, pfvector> bvpvector;
+        //Functions whose image is a vector s.t b(X), F(x), g(X), psi(X)
+        pfvector F, mu, b, psi;
 
-        //Sigma is the /sigma matrix
-        pfmatrix Sigma;
+        //sigma is the /sigma matrix
+        pfmatrix sigma;
+        
+        //The boundary is stored in surf
+        Boundary bound;
+
+        //The type of BC's is stored in stop
+        pfstop stop;
 
         //All the entries of the maps are set as Default. bvplat remains empty
         BVP(void);
 
-        //The object is properly initialized given the functions or the directorys where the
-        // Look At Tables are stored.
-        void BVP_init(std::map<std::string, pfscalar>,
-                    std::map<std::string, pfvector>,
-                    std::map<std::string, pfmatrix>,
-                    std::map<std::string, std::string>);
+        //The object is properly initialized given the functions or the directorys where the look up tables are stored.
+        void BVP_init(std::map<std::string, pfscalar> map_fscalar,
+                    std::map<std::string, pfvector> map_fvector,
+                    std::map<std::string, pfmatrix> map_fmatrix,
+                    std::map<std::string, std::string> map_lut);
+
         //Initialization of the surface variable
-        void Surf_init(pfbound);
-        void Surf_init(std::string);
+        void Surf_init(pfbound boundary, pfstop stopf);
+        void Surf_init(std::string boundary , pfstop stopf);
 
 };
 #endif
--- node.hpp
+++ node.hpp
@@ -1,19 +1,37 @@
 #ifndef NODE
 #define NODE
-
+//If we want to debug our code
+#define DEBUG
 #include <iostream>
 #include <map>
+#include <random>
 #include <eigen3/Eigen/Core>
 #include "lookuptable.hpp"
 #include "BVP.hpp"
 
 class Node{
-    private:
-        Eigen::VectorXf x0;
-        void UpdateStat(float &sum , float &sqsum, int &n, int sumsteps, int steps);
-    public:
 
-    float h, solution, tolerance, std, var, pearcoef, apl;
+    private:
+        //x0 stores the point where the solution is going to be computed 
+        Eigen::VectorXf x0;
+
+        //UpdateStat upgrades the statistic variables involved in the computation of the solution
+        void UpdateStat(float &sum , float &sqsum, float &, int &n, int &sumsteps, int steps);
+
+
+    public:
+    /*
+        -h stores the value of the time discretization
+        -solution stores the value of the solution in x0
+        -tolerance stores the tolerable statistic error 
+        (relative to the absolute error if solution available)
+        -std is the standard deviation
+        -var is the variance
+        -pearcoef measures the correlation between the solution
+         and ji;
+        -
+    */
+    float h, solution, tolerance, var, std, covar, pearcoef, apl;
     Node(void);
     void init (Eigen::VectorXf X_init, float tol, float discrezitation);
     void Solve(BVP bvp);
